c6c2ddcd4c5ad0f778ea8239bddd0b5816fb4197
amirKhani <ww.amirkhani.i@gmail.com>
2024-11-13 16:34:20 +0330

    update API

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import HomePageT from "@/components/templates/HomePageT";
 
 
 export default async  function Home() {
-   const res= await fetch("http://localhost:3100/food",{
+   const res= await fetch("https://json-server-beta-three.vercel.app/food",{
     cache:"force-cache"
    });
    const data=await res.json();
