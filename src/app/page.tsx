ee35bd936a00185336e72fbe18aa172a3531f6d9
amirKhani <ww.amirkhani.i@gmail.com>
2024-12-22 16:06:35 +0330

    update fetch config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,9 @@ import HomePageT from "@/components/templates/HomePageT";
 
 
 export default async  function Home() {
-   const res= await fetch("https://json-server-beta-three.vercel.app/food");
+   const res= await fetch("https://json-server-beta-three.vercel.app/food",{
+    cache:"force-cache"
+   });
    const data=await res.json();
   return (
   <HomePageT data={data}/>
